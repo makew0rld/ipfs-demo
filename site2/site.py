311ee12ae774b784b04938db49a85ded2840a003
makeworld <makeworld@protonmail.com>
2018-09-03 17:42:57 -0400

    new code

--- site2/site.py
+++ site2/site.py
@@ -13,31 +13,36 @@ from time import sleep
 # if it changes. If it does, that is when it sends the broadcast
 
 app = Flask(__name__, template_folder=".")
-app.config['SECRET_KEY'] = "nohackplzitsjustademoiknowitsopensourcebutcomeonman"
+app.config['SECRET_KEY'] = "demosocketkey"
 app.config
 socketio = SocketIO(app)
 
-ipfs = ""  # IPFS ref to file that IPNS address points to
 
 @app.route("/")
 def mainpage():
+
+    print(":: rendered homepage")
     return render_template("home.html")
 
 # The client sends a "success" message once it has connected
 @socketio.on("success")
 def on_success(message):
-    global ipfs
+    ipfs = ""  # IPFS ref to file that IPNS address points to
     # Now the client is connected, the IPNS polling can begin
+    print(":: Started update loop")
     while True:
         # Hardcoded demo key, from the key file included in the repo
         ipfs2 = subprocess.run(["ipfs", "name", "resolve", "QmbfT8139rgMeKZdLDgz45zQQVzCuKnHRtzohuT5aqcRV8"],
                                stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+        print(":: Resolved to:", ipfs2)
         # If the file has changed
         if ipfs != ipfs2:
             ipfs = ipfs2
-            # Tell the client - it will update the iframe img
-            emit("update", "update", broadcast=True)
-        sleep(1)
+            print(":: Updated the ipfs var")
+            # Tell the client - it will update the iframe img with the new url
+            emit("update", {"data": ipfs}, broadcast=True)
+            print(":: Told the client")
+        sleep(2)
 
 
 if __name__ == '__main__':
