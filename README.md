9d912c45bef5f852a7d1d080da05a6468b1e1ad8
makeworld <makeworld@protonmail.com>
2018-09-04 08:39:00 -0400

    updated readme and some code

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A offline, mesh-networking demo of IPFS, originally for use by tomesh.net
 ## Notes
 
 * Add nginx script so that it reverse proxies the flask server over IPv6 to port 80
-* It will be on /demo
+* It will be on /demo - an index file or something will redirect there from /
 * Install script involves importing demo key file
 * It's assumed that nodes 1 and 2 are peered over IPFS, and have the daemon started already
 * make /tmp/ipfs-demo folder as part of install
--- site2/site.py
+++ site2/site.py
@@ -2,15 +2,8 @@
 
 from flask import Flask, render_template
 import subprocess
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 from time import sleep
-
-# TODO - Plan:
-# Have a site with a iframe that leads to a /file url that displays an img
-# /file pulls from the :8080 gateway, using an already known file
-# Use socketIO to have the server tell the browser when to refresh the iframe
-# In the background, the server is resolving the IPNS hash constantly, to see
-# if it changes. If it does, that is when it sends the broadcast
 
 app = Flask(__name__, template_folder=".")
 app.config['SECRET_KEY'] = "demosocketkey"
@@ -23,6 +16,7 @@ def mainpage():
 
     print(":: rendered homepage")
     return render_template("home.html")
+
 
 # The client sends a "success" message once it has connected
 @socketio.on("success")
@@ -40,7 +34,7 @@ def on_success(message):
             ipfs = ipfs2
             print(":: Updated the ipfs var")
             # Tell the client - it will update the iframe img with the new url
-            emit("update", {"data": ipfs}, broadcast=True)
+            socketio.emit('update', {"data": ipfs})
             print(":: Told the client")
         sleep(2)
 
