77357a29e646effd357316499d5af25dce45fc73
makeworld <makeworld@protonmail.com>
2018-09-06 20:29:35 -0400

    switched to polling method

--- site2/site.py
+++ site2/site.py
@@ -13,30 +13,18 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def mainpage():
-
-    print(":: rendered homepage")
     return render_template("home.html")
 
 
-# The client sends a "success" message once it has connected
-@socketio.on("success")
-def on_success(message):
-    ipfs = ""  # IPFS ref to file that IPNS address points to
-    # Now the client is connected, the IPNS polling can begin
-    print(":: Started update loop")
-    while True:
-        # Hardcoded demo key, from the key file included in the repo
-        ipfs2 = subprocess.run(["ipfs", "name", "resolve", "QmbfT8139rgMeKZdLDgz45zQQVzCuKnHRtzohuT5aqcRV8"],
-                               stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
-        print(":: Resolved to:", ipfs2)
-        # If the file has changed
-        if ipfs != ipfs2:
-            ipfs = ipfs2
-            print(":: Updated the ipfs var")
-            # Tell the client - it will update the iframe img with the new url
-            socketio.emit('update', {"data": ipfs})
-            print(":: Told the client")
-        sleep(2)
+@socketio.on("update")
+def update():
+    print("Started update func")
+    # Hardcoded demo key, from the key file included in the repo
+    ipfs = subprocess.run(["ipfs", "name", "resolve", "QmbfT8139rgMeKZdLDgz45zQQVzCuKnHRtzohuT5aqcRV8"],
+                          stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+    # Tell the client - it will update the iframe img with the new url
+    print(":: Told the client")
+    return ipfs
 
 
 if __name__ == '__main__':
